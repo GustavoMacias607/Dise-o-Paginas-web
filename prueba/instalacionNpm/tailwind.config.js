f449d779c59a9add0cf71edc20e9e1082af479c9
GustavoMacias <gustavo.maci.1234@gmail.com>
2023-11-04 21:53:36 -0600

    practicando taildwind

---
+++ prueba/instalacionNpm/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        danger: '#ff5f40'
+      }
+    },
+  },
+  plugins: [],
+}
+
